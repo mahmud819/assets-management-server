42fd676b06cb502282e1c5229f7bd1b4617ebd52
mahmud819 <mahmudurr819@gmail.com>
2025-01-25 20:16:32 +0600

    products get function implement

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 5000;
   app.use(cors())
   app.use(express.json());
  
-// IlvKkoOVd2HLodCU
+
   
   // const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.tui29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.tui29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -39,6 +39,7 @@ const port = process.env.PORT || 5000;
       await client.connect();
       // !All collection
       const userCollection = client.db("assets-management").collection("users");
+      const productsCollection = client.db("assets-management").collection("products")
       // !All post operation
 
       app.post("/users", async (req, res) => {
@@ -47,12 +48,25 @@ const port = process.env.PORT || 5000;
         const result = await userCollection.insertOne(newUsers);
         res.send(result);
       });
+      
+      app.post('/products',async(req,res)=>{
+        const newProducts = req.body;
+        const result = await productsCollection.insertOne(newProducts);
+        res.send(result);
+      })
       // ! All get operation
       app.get("/users", async (req, res) => {
         const cursor = userCollection.find();
         const result = await cursor.toArray();
         res.send(result);
       });
+
+      app.get("/products", async (req, res) => {
+        const cursor = productsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+      
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
