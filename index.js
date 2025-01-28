0dceb82dd2bd922580ba32e3e09b0af8cc908b8f
mahmud819 <mahmudurr819@gmail.com>
2025-01-28 18:16:47 +0600

    jwt token create

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 
 
-// app.use(
-//     cors({
-//       origin: [
-//         "http://localhost:5173",
-       
-//       ],
-//       credentials: true,
-//     })
-//   );
-  app.use(cors())
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5000"
+    ],
+    credentials: true,
+  })
+);
+  // app.use(cors())
   app.use(express.json());
+  app.use(cookieParser());
  
 
+  const verifyToken = (req, res, next) => {
+    console.log(req.cookies);
+    const token = req.cookies.token;
+    console.log("inside verify midle ware", token);
+    if (!token) {
+      return res.status(401).send({ Message: "Unauthorized Access" });
+    }
   
-  // const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.tui29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).send({ message: "UnAutherized Access" });
+      }
+      req.user = decoded;
+      next();
+    });
+  };
+  
   const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.tui29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
   
   
@@ -49,7 +68,30 @@ const port = process.env.PORT || 5000;
         const result = await userCollection.insertOne(newUsers);
         res.send(result);
       });
+
+      app.post("/jwt", async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "5h" });
+        res
+          .cookie("token", token, {
+            httpOnly: true,
+            // secure: process.env.NODE_ENV === 'production',
+            secure: false,
+          })
+          .send({ success: true });
+      });
       
+      app.post("/logOut", (req, res) => {
+        res
+          .clearCookie("token", {
+            httpOnly: true,
+            // secure: process.env.NODE_ENV === 'production',
+            secure: false,
+          })
+          .send({ success: true });
+      });
+  
+
       app.post('/products',async(req,res)=>{
         const newProducts = req.body;
         const result = await productsCollection.insertOne(newProducts);
@@ -73,7 +115,55 @@ const port = process.env.PORT || 5000;
         const result = await cursor.toArray();
         res.send(result);
       });
+
+      app.get("/products", async (req, res) => {
+        const productName = req.query.productName;
+        const query = {productName: {$regex: productName, $options: 'i'},};
+        console.log(productName,query);
+        const result = await productsCollection.find(query).toArray()
+
+        // const {inputValue} = req.query;
+        // const result = await productsCollection.find({productName : {$regex : inputValue,$options : 'i'} }).toArray();
+
+        res.send(result);
+      });
+
+      app.get("/requestedProducts", async (req, res) => {
+        const cursor = requestedProductsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
       
+
+      app.get('/requestedProducts/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await requestedProductsCollection.findOne(query);
+        res.send(result);
+      })
+      // ! put operatio
+
+      app.patch('/requestedProducts/:id',async(req,res)=>{
+        const id= req.params.id;
+        try{
+
+          const query = {_id : new ObjectId(id)}
+          const updatedRequestedProducts = req.body;
+        //   console.log('requested id :',query);
+        // console.log('update',req.body);
+          const updateProducts = await requestedProductsCollection.findOneAndUpdate(
+            query,
+            {$set : updatedRequestedProducts},
+            {returnDocument : after}
+          );
+          console.log('updadedData',updateProducts)
+          res.json({updateProducts});
+        }
+        catch(error){
+          res.status(500).json({error : 'faild to aprrove'});
+        }
+      
+      });
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
